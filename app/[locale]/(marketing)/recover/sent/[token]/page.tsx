67cfdac4a15cdce1e326d885c1ff3733f1dd7216
Deniz Gunes <denizlg24@gmail.com>
2025-11-06 23:44:51 +0000

    fix(auth): email was failing because of file access. fixed password to use tokens

--- app/[locale]/(marketing)/recover/sent/[token]/page.tsx
+++ app/[locale]/(marketing)/recover/sent/[token]/page.tsx
@@ -1,20 +1,29 @@
 import { Button } from "@/components/ui/button";
 import { Link } from "@/i18n/navigation";
+import { ResetToken } from "@/models/auth/ResetToken";
+import { User } from "@/models/auth/User";
+import env from "@/utils/env";
 import { LogIn, MailCheck } from "lucide-react";
 import { setRequestLocale } from "next-intl/server";
-import { use } from "react";
+import { notFound } from "next/navigation";
 
-export default function Home({
+export default async function Home({
   params,
 }: {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  params: any;
+  params: Promise<{ locale: string; token: string }>;
 }) {
-  const { locale, email } = use<{
-    locale: string;
-    email: string;
-  }>(params);
+  const { locale, token } = await params;
   setRequestLocale(locale);
+  const foundResetToken = await ResetToken.findOne({
+    token: `${token}${env.EMAIL_TOKEN_SUFFIX}`,
+  }).lean();
+  if (!foundResetToken) {
+    notFound();
+  }
+  const user = await User.findOne({ sub: foundResetToken.sub }).lean();
+  if (!user) {
+    notFound();
+  }
 
   return (
     <main className="flex flex-col items-center w-full mx-auto md:gap-0 gap-2 mb-16">
@@ -25,8 +34,7 @@ export default function Home({
             We&apos;ve sent you an email!
           </h1>
           <h2 className="lg:text-base sm:text-sm text-xs font-normal text-left">
-            Check your inbox{" "}
-            <span className="font-semibold">{decodeURIComponent(email)}</span>{" "}
+            Check your inbox <span className="font-semibold">{user.email}</span>{" "}
             and get a link to reset your password.
           </h2>
           <Button asChild className="w-full">
