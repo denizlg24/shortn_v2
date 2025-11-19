93174bbcd8fad5c4803b2a534e01e734febede09
denizlg24 <denizlg24@gmail.com>
2025-11-19 15:42:50 +0000

    feat(link-in-bio): made bio model and imported new shadcn components

--- app/[locale]/dashboard/pages/page.tsx
+++ app/[locale]/dashboard/pages/page.tsx
@@ -1,19 +1,24 @@
+import { auth } from "@/auth";
+import { redirect } from "@/i18n/navigation";
+import { connectDB } from "@/lib/mongodb";
+import { BioPage } from "@/models/link-in-bio/BioPage";
 import { setRequestLocale } from "next-intl/server";
-import { use } from "react";
 
-export default function Home({
+export default async function Home({
   params,
 }: {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  params: any;
+  params: Promise<{ locale: string }>;
 }) {
-  const { locale } = use<{
-    locale: string;
-  }>(params);
+  const { locale } = await params;
   setRequestLocale(locale);
+  const session = await auth();
+  if (!session?.user) {
+    redirect({ href: "login", locale });
+  }
+  await connectDB();
+  const bios = await BioPage.find({ userId: session!.user.sub }).lean();
+  console.log(bios);
   return (
-    <main className="flex flex-col items-center w-full mx-auto md:gap-0 gap-2 bg-accent px-4 sm:pt-14! pt-6! pb-16">
-      PAGES
-    </main>
+    <main className="flex flex-col items-center w-full mx-auto md:gap-0 gap-2 bg-accent px-4 sm:pt-14! pt-6! pb-16"></main>
   );
 }
---
+++ models/link-in-bio/BioPage.ts
@@ -0,0 +1,98 @@
+import mongoose, { Schema, Document, Model } from "mongoose";
+import UrlV3, { IUrl } from "../url/UrlV3";
+import { ok } from "assert";
+
+ok(UrlV3);
+
+export interface IHeader extends Document {
+  headerStyle: "centered" | "left-aligned" | "right-aligned";
+  headerImageUrl?: string;
+  headerImageStyle: "square" | "rounded" | "circle";
+  headerBackgroundImage?: string;
+  headerBackgroundColor: string;
+  headerTitle?: string;
+  headerSubtitle?: string;
+}
+
+export interface IBioPage extends Document {
+  userId: string;
+  slug: string;
+  title?: string;
+  description?: string;
+  avatarUrl?: string;
+  theme?: {
+    primaryColor?: string;
+    background?: string;
+    textColor?: string;
+    buttonStyle?: "rounded" | "square" | "pill";
+    header?: IHeader;
+  };
+  links: { link: mongoose.Types.ObjectId | IUrl; image?: string }[];
+  socials?: {
+    instagram?: string;
+    twitter?: string;
+    github?: string;
+    linkedin?: string;
+  };
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const HeaderSchema: Schema = new Schema({
+  headerStyle: {
+    type: String,
+    enum: ["centered", "left-aligned", "right-aligned"],
+    default: "centered",
+  },
+  headerImageUrl: { type: String },
+  headerImageStyle: {
+    type: String,
+    enum: ["square", "rounded", "circle"],
+    default: "circle",
+  },
+  headerBackgroundImage: { type: String },
+  headerBackgroundColor: { type: String, default: "#ffffff" },
+  headerTitle: { type: String },
+  headerSubtitle: { type: String },
+});
+
+const BioPageSchema: Schema = new Schema(
+  {
+    userId: { type: String, required: true, index: true },
+    slug: { type: String, required: true, unique: true },
+    title: { type: String },
+    description: { type: String },
+    avatarUrl: { type: String },
+    theme: {
+      primaryColor: { type: String },
+      background: { type: String },
+      textColor: { type: String },
+      buttonStyle: {
+        type: String,
+        enum: ["rounded", "square", "pill"],
+        default: "rounded",
+      },
+      header: { type: HeaderSchema },
+    },
+    links: [
+      {
+        link: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "UrlV3",
+          required: true,
+        },
+        image: { type: String },
+      },
+    ],
+    socials: {
+      instagram: { type: String },
+      twitter: { type: String },
+      github: { type: String },
+      linkedin: { type: String },
+    },
+  },
+  { timestamps: true },
+);
+
+export const BioPage: Model<IBioPage> =
+  mongoose.models.BioPage || mongoose.model<IBioPage>("BioPage", BioPageSchema);
